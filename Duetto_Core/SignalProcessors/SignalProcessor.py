93cba4b67720b98ec5f5fbeea441e07c64848f4c
Carlos <c.pena@lab.matcom.uh.cu>
2013-12-23 14:33:27 -0800

    Arreglando codigo interno

--- Duetto_Core/SignalProcessors/SignalProcessor.py
+++ Duetto_Core/SignalProcessors/SignalProcessor.py
@@ -20,33 +20,31 @@ class SignalProcessor:
     signal = property(_getSignal, _setSignal)
     #endregion
 
-
-    def rms(self,indexFrom=0,indexTo=-1):
+    def rms(self, indexFrom=0, indexTo=-1):
         """
         computes the root mean square of the signal.
         indexFrom,indexTo the optionally limits of the interval
         """
-
-        if(indexTo==-1):
-            indexTo=len(self.signal.data)
-        n=indexTo-indexFrom
-        globalSum=0.0
-        intervalSum=0.0
+        if indexTo == -1:
+            indexTo = len(self.signal.data)
+        n = indexTo-indexFrom
+        globalSum = 0.0
+        intervalSum = 0.0
         for i in range(n):
-            intervalSum+=(self.signal.data[indexFrom+i]**2)
-            if(i%10==0):
-                globalSum+=intervalSum*1.0/n
-                intervalSum=0.0
+            intervalSum += (self.signal.data[indexFrom+i]**2)
+            if i % 10 == 0:
+                globalSum += intervalSum * 1.0 / n
+                intervalSum = 0.0
 
-        globalSum+=intervalSum*1.0/n
+        globalSum += intervalSum * 1.0 / n
         return sqrt(globalSum)
 
-    def checkIndexes(self,indexFrom,indexTo):
+    def checkIndexes(self, indexFrom, indexTo):
         """
         check that the specified indexes are in the range of the signal data
         """
-        dataLength=len(self.signal.data)
-        if(indexFrom>indexTo or indexFrom>=dataLength or indexTo>dataLength):
+        dataLength = len(self.signal.data)
+        if indexFrom > indexTo or indexFrom >= dataLength or indexTo > dataLength:
             raise IndexError()
 
     def envelope(self, n):
