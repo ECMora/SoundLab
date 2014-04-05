57d582aa26c8402b0d28338cce6cdfbc1e7ca3b8
Gabriela Perez Alvarez <g.perez@lab.matcom.uh.cu>
2014-04-05 14:20:54 +0200

    changes

---
+++ duetto_core/Segmentation/Detectors/FeatureExtractionDetectors/HarmonicDetector.py
@@ -0,0 +1,25 @@
+from matplotlib import mlab
+
+from Duetto_Core.Segmentation.Detectors.MinMaxFrequencyDetector import MinMaxFrequencyDetector
+from Duetto_Core.Segmentation.Detectors import Detector
+from Duetto_Core.Segmentation.Detectors.FeatureExtractionDetectors import FrequencyPeeksDetector
+from Duetto_Core.Segmentation.Elements.OneDimensionalElement import OneDimensionalElement
+
+
+class HarmonicDetector(Detector):
+    def __init__(self):
+        Detector.__init__(self)
+
+    def detect(self,signal,indexFrom=0,indexTo=-1,thresholdAbs = 0,thresholdRel = 0,hysteresis = 0):
+        fpeeksd = FrequencyPeeksDetector()
+        fpeeksd.detect(signal,indexFrom,indexTo,thresholdAbs,hysteresis)
+        if indexTo == -1:
+            indexTo = signal.data.size
+        pxx,freqs = mlab.psd(signal.data[indexFrom:indexTo],Fs=signal.samplingRate,NFFT=512)
+        self.oneDimensionalElements = []
+        minmaxfreqd = MinMaxFrequencyDetector()
+        for p in fpeeksd.pointers:
+            minmaxfreqd.detect(signal,indexFrom,indexTo,thresholdRel,p.index)
+            interval = minmaxfreqd.intervals[0]
+            self.oneDimensionalElements.append(OneDimensionalElement(signal,interval.min,interval.max))
+
