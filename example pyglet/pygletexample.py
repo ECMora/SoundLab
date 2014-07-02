5f8fceef2dc9db4f8d72583f65aa65057f7ea274
Yasel <y.febles@lab.matcom.uh.cu>
2014-07-02 10:55:13 +0200

    CAMBIOS

--- example pyglet/pygletexample.py
+++ example pyglet/pygletexample.py
@@ -1,145 +1,178 @@
 # -*- coding: utf-8 -*-
-"""
-Demonstrates basic use of LegendItem
-"""
-from PyQt4.QtGui import QFont
+#!/usr/bin/env pythonimport sys
 
-import pyqtgraph as pg
-import numpy as np
-from pyqtgraph.Qt import QtCore, QtGui
+# Major library imports
+try:
+    import pyaudio
+except ImportError:
+    sys.exit('You need to install pyaudio to installed to run this demo.')
 
+from numpy import zeros, linspace, short, fromstring, hstack, transpose
+from scipy import fft
 
+# Enthought library imports
+from chaco.default_colormaps import jet
+from enable.api import Component, ComponentEditor
+from traits.api import HasTraits, Instance
+from traitsui.api import Item, Group, View, Handler
+from pyface.timer.api import Timer
 
-class OscXAxis(pg.AxisItem):
-    def __init__(self, parent, *args, **kwargs):
-        pg.AxisItem.__init__(self, *args, **kwargs)
-        self.parent = parent
-        font = QFont(self.font)
-        font.setPointSize(20)
-        self.setTickFont(font)
+# Chaco imports
+from chaco.api import Plot, ArrayPlotData, HPlotContainer
 
-    def tickStrings(self, values, scale, spacing):
-        strns = []
-        delta = spacing / self.parent.signalProcessor.signal.samplingRate
-        a = max(-(int(np.log10(delta)) - 1), 0)
-        a = min(a, 4)
-        s = "{:." + str(a) + "f}"
-        for x in values:
-            strns.append(s.format(x * 1.0 / self.parent.signalProcessor.signal.samplingRate))
-        return strns
+NUM_SAMPLES = 1024
+SAMPLING_RATE = 11025
+SPECTROGRAM_LENGTH = 100
 
-    def tickValues(self, minVal, maxVal, size):
-        minVal = self.parent.mainCursor.min
-        maxVal = self.parent.mainCursor.max
-        spacing = self.tickSpacing(minVal, maxVal, size)[0][0]
-        values = []
-        temp = minVal
-        while (temp < maxVal):
-            values.append(temp)
-            temp += spacing
-        return [(spacing, values)]
+#============================================================================
+# Create the Chaco plot.
+#============================================================================
 
-    def tickSpacing(self, minVal, maxVal, size):
-        minVal = self.parent.mainCursor.min
-        maxVal = self.parent.mainCursor.max
-        return [(max((maxVal - minVal) / (10.0 * self.parent.signalProcessor.signal.samplingRate),
-                     0.0001) * self.parent.signalProcessor.signal.samplingRate, 0)]
+def _create_plot_component(obj):
+    # Setup the spectrum plot
+    frequencies = linspace(0.0, float(SAMPLING_RATE)/2, num=NUM_SAMPLES/2)
+    obj.spectrum_data = ArrayPlotData(frequency=frequencies)
+    empty_amplitude = zeros(NUM_SAMPLES/2)
+    obj.spectrum_data.set_data('amplitude', empty_amplitude)
 
+    obj.spectrum_plot = Plot(obj.spectrum_data)
+    obj.spectrum_plot.plot(("frequency", "amplitude"), name="Spectrum",
+                           color="red")
+    obj.spectrum_plot.padding = 50
+    obj.spectrum_plot.title = "Spectrum"
+    spec_range = obj.spectrum_plot.plots.values()[0][0].value_mapper.range
+    spec_range.low = 0.0
+    spec_range.high = 5.0
+    obj.spectrum_plot.index_axis.title = 'Frequency (hz)'
+    obj.spectrum_plot.value_axis.title = 'Amplitude'
 
-def getImage(widget):
-    image = QtGui.QPixmap.grabWindow(widget.winId())
-    image.save("Local.jpg", 'jpg')
+    # Time Series plot
+    times = linspace(0.0, float(NUM_SAMPLES)/SAMPLING_RATE, num=NUM_SAMPLES)
+    obj.time_data = ArrayPlotData(time=times)
+    empty_amplitude = zeros(NUM_SAMPLES)
+    obj.time_data.set_data('amplitude', empty_amplitude)
 
-if __name__ == '__main__':
-    import sys
+    obj.time_plot = Plot(obj.time_data)
+    obj.time_plot.plot(("time", "amplitude"), name="Time", color="blue")
+    obj.time_plot.padding = 50
+    obj.time_plot.title = "Time"
+    obj.time_plot.index_axis.title = 'Time (seconds)'
+    obj.time_plot.value_axis.title = 'Amplitude'
+    time_range = obj.time_plot.plots.values()[0][0].value_mapper.range
+    time_range.low = -0.2
+    time_range.high = 0.2
 
-    plt = pg.plot( axisItems={'bottom':  OscXAxis(None, orientation='bottom'), 'left':OscXAxis(None, orientation='left') })
-    font = QFont()
-    font.setPointSize(15)
-    ax = plt.plotItem.getAxis('bottom')
-    ax.setTickFont(font)
-    ax.setLabel(text="<h1>Time (ms)</h1>")
-    ax = plt.plotItem.getAxis('left')
-    ax.setTickFont(font)
-    plt.setWindowTitle('')
-    #plt.addLegend()
-    plt.plotItem.showGrid(True,True)
+    # Spectrogram plot
+    spectrogram_data = zeros(( NUM_SAMPLES/2, SPECTROGRAM_LENGTH))
+    obj.spectrogram_plotdata = ArrayPlotData()
+    obj.spectrogram_plotdata.set_data('imagedata', spectrogram_data)
+    spectrogram_plot = Plot(obj.spectrogram_plotdata)
+    max_time = float(SPECTROGRAM_LENGTH * NUM_SAMPLES) / SAMPLING_RATE
+    max_freq = float(SAMPLING_RATE / 2)
+    spectrogram_plot.img_plot('imagedata',
+                              name='Spectrogram',
+                              xbounds=(0, max_time),
+                              ybounds=(0, max_freq),
+                              colormap=jet,
+                              )
+    range_obj = spectrogram_plot.plots['Spectrogram'][0].value_mapper.range
+    range_obj.high = 5
+    range_obj.low = 0.0
+    spectrogram_plot.title = 'Spectrogram'
+    obj.spectrogram_plot = spectrogram_plot
 
+    container = HPlotContainer()
+    container.add(obj.spectrum_plot)
+    container.add(obj.time_plot)
+    container.add(spectrogram_plot)
 
-    local_max_match = [95.39, 91.01, 98.67, 96.21, 86.42, 100, 91.53, 88.0, 93.73, 87.5, 90.63, 84.07, 86.46, 71.73, 91.12, 98.78, 71, 86.67, 91.14, 80.15, 99.47, 88.41, 94.69, 98.47, 99.76, 96.26, 99.08, 100, 72.16, 94.31]
+    return container
 
-    local_max_error =  [0.95, 2.85, 7.09, 0.63, 3.04, 17.86, 6.38, 10.7, 0.86, 18.38, 1.59, 6.44, 3.02, 0.12, 0.12, 0.63, 0, 0, 0.41, 0, 2.37, 0.01, 4.81, 4.06, 0.01, 1.68, 4.2, 10.35, 0.68, 4.09]
-
-    interval_match = [90.51, 84.46, 100, 100, 96.46, 94.61, 97.24, 100, 98.1, 65.81, 80.02, 80.74, 92.11, 60.71, 91.25, 99.67, 72.64, 96.3, 84.85, 82.17, 81.74, 83.88, 69, 82.4, 99.75, 88.71, 98.73, 100, 71.3, 95.97]
-
-    interval_error =[2.86, 6.29, 19.53, 6.06, 12.78, 9.35, 39.28, 32, 2.15, 36.52, 5.58, 29.83, 16.22, 0, 6.55, 1.48, 0.01, 1.41, 0.47, 0.03, 2.99, 0, 0.7, 0.38, 0.15, 3.78, 4.44, 11.28, 0.23, 0]
-
-    envelope_match = [98.03, 96.34, 99.78, 100, 93.48, 100, 98.74, 98.67, 99.83, 96.74, 99.95, 85.45, 92.16, 84.51, 95.52, 99.64, 97.61, 97.74, 96.87, 86.63, 99.47, 93.65, 95.7, 97, 99.79, 95.84, 99.65, 100, 73.63, 93.75]
-
-    envelope_error =[1.54, 7.51, 13.18, 3.38, 4.7, 17.86, 16.96, 24.22, 2.3, 18.93, 3.93, 5.91, 6.77, 0.14, 2.21, 0.95, 0.3, 0, 0.47, 0, 5.68, 0.01, 3.29, 1.49, 0, 0.92, 6, 11.45, 0.09, 3.93]
-
-    intervals_analysis_bats_match = [98.99, 99.2, 99.25, 98.75, 98.84, 98.84, 99.14, 98.96, 98.96, 98.91, 98.79, 98.14, 98.64, 98.46, 97.43, 97.88, 98.5, 97.78, 98.02, 98.67]
-
-    intervals_analysis_bats_error =[8.18, 11.26, 15.67, 19.56, 23.66, 23.66, 34.55, 38.46, 38.46, 48.42, 51.27, 58.74, 61.18, 69.19, 71.27, 79.47, 84.93, 96.47, 118.8, 157.81]
-
-    intervals_analysis_birds_match = [80.95,98.69,90.82,99.15,99.15,99.13,99.11,99.12,99.10,80.04]
-
-    intervals_analysis_birds_error =[27.82,8.97,1.66,9.28,9.28,9.85,9.43,9.71,10.07,0.44]
-
-    #a  = []
-    #for x in range(1,len(intervals_analysis_birds_match)+1):
-    #    a.append(intervals_analysis_birds_match[x-1])
-    #    a.append((intervals_analysis_birds_match[x-1]+intervals_analysis_birds_match[x])/2.0)
-
-    #print(len(a))
-    rang = np.arange(1,31)
-
-    plt.setRange(xRange=(0,30),
-                                         yRange=(-1,110))
-
-
-    l = pg.LegendItem()  # args are (size, offset)
-    l.setParentItem(plt.graphicsItem())   # Note we do NOT call plt.addItem in this case
+_stream = None
+def get_audio_data():
+    global _stream
+    if _stream is None:
+        pa = pyaudio.PyAudio()
+        _stream = pa.open(format=pyaudio.paInt16, channels=1, rate=SAMPLING_RATE,
+                     input=True, frames_per_buffer=NUM_SAMPLES)
+    audio_data  = fromstring(_stream.read(NUM_SAMPLES), dtype=short)
+    normalized_data = audio_data / 32768.0
+    return (abs(fft(normalized_data))[:NUM_SAMPLES/2], normalized_data)
 
 
-    #
-    #LocalMaxMatch= plt.plot(rang,local_max_match, pen='g', name='Match % ',symbol='o',symbolPen='0F0',symbolBrush='AAA')
-    #LocalMaxError = plt.plot(rang,local_max_error, pen='r', name='Difference % ',symbol='o',symbolPen='F00',symbolBrush='AAA')
-    #l.addItem(LocalMaxMatch,u"<h1> Local Max Method Match %</h1>")
-    #l.addItem(LocalMaxError,u"<h1> Local Max Method Difference %</h1>")
+# HasTraits class that supplies the callable for the timer event.
+class TimerController(HasTraits):
 
-    #IntervalMatch = plt.plot(rang,interval_match, pen='g', name=' Match %',symbol='o',symbolPen='0F0',symbolBrush='AAA')
-    #IntervalError = plt.plot(rang,interval_error, pen='r', name=' Difference %',symbol='o',symbolPen='F00',symbolBrush='AAA')
-    #l.addItem(IntervalMatch,u"<h1> Interval RMS Method Match %</h1>")
-    #l.addItem(IntervalError,u"<h1> Interval RMS Method Difference %</h1>")
+    def onTimer(self, *args):
+        spectrum, time = get_audio_data()
+        self.spectrum_data.set_data('amplitude', spectrum)
+        self.time_data.set_data('amplitude', time)
+        spectrogram_data = self.spectrogram_plotdata.get_data('imagedata')
+        spectrogram_data = hstack((spectrogram_data[:,1:],
+                                   transpose([spectrum])))
 
-    EnvelopeMatch = plt.plot(rang,envelope_match, pen='g', name='Match %',symbol='o',symbolPen='0F0',symbolBrush='AAA')
-    EnvelopeError= plt.plot(rang,envelope_error, pen='r', name='Difference %',symbol='o',symbolPen='F00',symbolBrush='AAA')
-    l.addItem(EnvelopeMatch,u"<h1> Envelope Method Match %</h1>")
-    l.addItem(EnvelopeError,u"<h1>Envelope Method Difference %</h1>")
+        self.spectrogram_plotdata.set_data('imagedata', spectrogram_data)
+        self.spectrum_plot.request_redraw()
+        return
 
-    #
-    #rang = np.arange(1,len(intervals_analysis_bats_match)+1)
-    #rang = rang/2.0
-    #plt.setRange(xRange=(0,rang.size/2),yRange=(0,120))
-    #Intervals_Analysis_Bats_Match = plt.plot(rang,intervals_analysis_bats_match, pen='g', name='Match %',symbol='o',symbolPen='0F0',symbolBrush='AAA')
-    #Intervals_Analysis_Bats_Error= plt.plot(rang,intervals_analysis_bats_error, pen='r', name='Difference %',symbol='o',symbolPen='F00',symbolBrush='AAA')
-    #l.addItem(Intervals_Analysis_Bats_Match,u"<h1> Interval RMS (Bats) Match%</h1>")
-    #l.addItem(Intervals_Analysis_Bats_Error,u"<h1>Interval RMS (Bats) Difference %</h1>")
-    #
-    #rang = np.arange(1,len(intervals_analysis_birds_match)+1)
-    #plt.setRange(xRange=(0,rang.size),yRange=(0,120))
-    #Intervals_Analysis_Birds_Match = plt.plot(rang,intervals_analysis_birds_match, pen='g', name='Match %',symbol='o',symbolPen='0F0',symbolBrush='AAA')
-    #Intervals_Analysis_Birds_Error= plt.plot(rang,intervals_analysis_birds_error, pen='r', name='Difference %',symbol='o',symbolPen='F00',symbolBrush='AAA')
-    #l.addItem(Intervals_Analysis_Birds_Match,u"<h1> Interval RMS (Birds) Match%</h1>")
-    #l.addItem(Intervals_Analysis_Birds_Error,u"<h1>Interval RMS (Birds) Difference %</h1>")
-    #
+#============================================================================
+# Attributes to use for the plot view.
+size = (1900,900)
+title = "Audio Spectrum"
 
+#============================================================================
+# Demo class that is used by the demo.py application.
+#============================================================================
 
-    ## Start Qt event loop unless running in interactive mode or using pyside.
+class DemoHandler(Handler):
 
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    def closed(self, info, is_ok):
+        """ Handles a dialog-based user interface being closed by the user.
+        Overridden here to stop the timer once the window is destroyed.
+        """
 
-        getImage(plt)
+        info.object.timer.Stop()
+        return
 
-        QtGui.QApplication.instance().exec_()
+class Demo(HasTraits):
+
+    plot = Instance(Component)
+
+    controller = Instance(TimerController, ())
+
+    timer = Instance(Timer)
+
+    traits_view = View(
+                    Group(
+                        Item('plot', editor=ComponentEditor(size=size),
+                             show_label=False),
+                        orientation = "vertical"),
+                    resizable=True, title=title,
+                    width=size[0], height=size[1],
+                    handler=DemoHandler
+                    )
+
+    def __init__(self, **traits):
+        super(Demo, self).__init__(**traits)
+        self.plot = _create_plot_component(self.controller)
+
+    def edit_traits(self, *args, **kws):
+        # Start up the timer! We should do this only when the demo actually
+        # starts and not when the demo object is created.
+        self.timer = Timer(20, self.controller.onTimer)
+        return super(Demo, self).edit_traits(*args, **kws)
+
+    def configure_traits(self, *args, **kws):
+        # Start up the timer! We should do this only when the demo actually
+        # starts and not when the demo object is created.
+        self.timer = Timer(20, self.controller.onTimer)
+        return super(Demo, self).configure_traits(*args, **kws)
+
+popup = Demo()
+
+if __name__ == "__main__":
+    try:
+        popup.configure_traits()
+    finally:
+        if _stream is not None:
+            _stream.close()
