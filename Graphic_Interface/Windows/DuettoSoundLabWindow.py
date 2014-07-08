7ed9d13ced09112b1745cc600abb96a328db4767
Gaby <use@mail>
2014-07-08 16:48:30 +0200

    done

--- Graphic_Interface/Windows/DuettoSoundLabWindow.py
+++ Graphic_Interface/Windows/DuettoSoundLabWindow.py
@@ -759,7 +759,7 @@ class DuettoSoundLabWindow(QtGui.QMainWindow, Ui_DuettoMainWindow):
     def on_actionPower_Spectrum_triggered(self):
         minx = self.widget.zoomCursor.min
         maxx = max(self.widget.zoomCursor.max, min(minx + self.NFFT_pow, len(self.widget.signalProcessor.signal.data)))
-        dg_pow_spec = PowerSpectrumWindow(self, self.pow_spec_minY,self.pow_spec_maxY,self.pow_spec_lines,self.widget.signalProcessor.signal.data[minx:maxx],self.widget.signalProcessor.signal.samplingRate,)
+        dg_pow_spec = PowerSpectrumWindow(self, self.pow_spec_minY,self.pow_spec_maxY,self.pow_spec_lines,self.widget.signalProcessor.signal.data[minx:maxx],self.widget.signalProcessor.signal.samplingRate)
         dg_pow_spec.load_Theme(self.defaultTheme)
         self.pow_spec_windows.append(dg_pow_spec)
 
