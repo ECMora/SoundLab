37dd1657e74344b9b25b18c50e43d594cbc806bd
y.febles <y.febles@citmatel.inf.cu>
2014-11-18 15:55:12 -0500

    Sepracion de las herramientas de trabajo visual mediante jerarqui de clases.  Implementacion de la separacion provisional de las herramientas de los controles

--- graphic_interface/widgets/signal_visualizer_tools/PointerCursorTool.py
+++ graphic_interface/widgets/signal_visualizer_tools/PointerCursorTool.py
@@ -1,7 +1,77 @@
 # -*- coding: utf-8 -*-
-from graphic_interface.widgets.signal_visualizer_tools import SignalVisualizerTool
+from PyQt4 import QtCore
+import pyqtgraph as pg
+from PyQt4.QtGui import QCursor
+from graphic_interface.widgets.signal_visualizer_tools.SignalVisualizerTool import SignalVisualizerTool
 
 
 class PointerCursorTool(SignalVisualizerTool):
-    pass
+
+    #CONSTANTS
+    def __init__(self, widget):
+        SignalVisualizerTool.__init__(self, widget)
+        self.pointerCursor = pg.ScatterPlotItem()
+        self.last = {'pos': [0, 0], 'pen': {'color': 'w', 'width': 2}, 'brush': pg.intColor(255, 255), 'symbol': '+',
+                     'size': 20}
+
+    def mouseMoveEvent(self, event):
+        x = self.fromCanvasToClient(event.x())
+        y = self.fromCanvasToClientY(event.y())
+        info = self.getAmplitudeTimeInfo(x, y)
+        info0 = round(info[0], self.DECIMAL_PLACES), round(info[1], self.DECIMAL_PLACES)
+        if x == -1 or y == -1:
+            self.widget.setCursor(QCursor(QtCore.Qt.ArrowCursor))
+            return
+        if not self.mousePressed:
+            info = self.getAmplitudeTimeInfo(self.last['pos'][0], self.last['pos'][1])
+            info0 = round(info0[0], self.DECIMAL_PLACES), round(info0[1], self.DECIMAL_PLACES)
+            data = str.format('t0: {0}s  t1: {1}s  dt: {2}s          ' + str('Amp') + ': {3}%',
+                              info0[0], info[0],info[0] - info0[0], info[1])
+        else:
+            data = str.format(str('Time:') + ' {0}s          ' \
+                            + str('Amp') + ': {1}%', info[0], info[1])
+
+        self.widget.setCursor(QCursor(QtCore.Qt.CrossCursor))
+
+    def mousePressEvent(self, event):
+        self.mousePressed = True
+        self.pointerCursor.clear()
+        x = self.fromCanvasToClient(event.x())
+        y = self.fromCanvasToClientY(event.y())
+        if x == -1 or y == -1:
+            self.widget.setCursor(QCursor(QtCore.Qt.ArrowCursor))
+            return
+        self.last = {'pos': [x, y], 'pen': {'color': 'w', 'width': 2}, 'brush': pg.intColor(255, 255), 'symbol': '+',
+                     'size': 20}
+        self.pointerCursor.addPoints([self.last])
+
+    def mouseDoubleClickEvent(self, event):
+        pass
+
+    def mouseReleaseEvent(self, event):
+        self.mousePressed = False
+
+    def fromCanvasToClientY(self, yPixel):
+        """
+        Translates the coordinates from the canvas to its corresponding  index in the signal array
+        """
+        vb = self.widget.getPlotItem().getViewBox()
+        miny = vb.y()
+        maxy = vb.height() + miny
+        a, b = self.widget.getPlotItem().viewRange()[1]
+        yPixel = maxy - yPixel
+        if yPixel < miny:
+            self.pointerCursor.clear()
+            if not self.mousePressed:
+                self.pointerCursor.addPoints([self.last])
+            return -1
+        if yPixel > maxy:
+            self.pointerCursor.clear()
+            if not self.mousePressed:
+                self.pointerCursor.addPoints([self.last])
+            return -1
+        return a + int(round((yPixel - miny) * (b - a) * 1. / (maxy - miny), 0))
+
+    def dispose(self):
+        pass
 
