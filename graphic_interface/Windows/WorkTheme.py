16d85267b8bcb80e296350e53b9dd03bb2515510
Carlos <c.pena@lab.matcom.uh.cu>
2014-12-03 11:55:37 -0500

    working on WorkTheme:
     - changed WorkTheme to separate the oscillogram, spectrogram and power spectrum's themes (the class WorkTheme contains an instance of each of this other classes, so everything is still stored in just one file)
     - removed min and max y from theme (TODO: must change the way they are changed because right now you can't change them)

--- graphic_interface/Windows/WorkTheme.py
+++ graphic_interface/Windows/WorkTheme.py
@@ -1,33 +1,47 @@
-# -*- coding: utf-8 -*-
+class OscillogramTheme:
+    def __init__(self, background_color="000", plot_color="CC3", gridX=True, gridY=True):  # , minY=-100, maxY=100):
+        self.background_color = background_color
+        self.plot_color = plot_color
+        self.gridX = gridX
+        self.gridY = gridY
+        # self.minY = minY
+        # self.maxY = maxY
 
 
-class SerializedData:
+class SpectrogramTheme:
+    def __init__(self, background_color="000", gridX=True, gridY=True, colorbar=None, histRange=(-40, 0)):  # ,minY=0, maxY=22,
+        self.gridX = gridX
+        self.gridY = gridY
+        # self.minY = minY
+        # self.maxY = maxY
+        self.background_color = background_color
+        self.colorBarState = colorbar if colorbar is not None else {
+            'ticks': [(0.3333, (185, 0, 0, 255)), (0.6666, (255, 220, 0, 255)), (1, (255, 255, 255, 255)),
+                      (0, (0, 0, 0, 255))],
+            'mode': 'rgb'}
+        self.histRange = histRange
+
+
+class PowerSpectrumTheme:
+    def __init__(self, background_color="000", plot_color="FFF", gridX=True, gridY=True):
+        self.background_color = background_color
+        self.plot_color = plot_color
+        self.gridX = gridX
+        self.gridY = gridY
+
+
+class WorkTheme:
     """
     The options to customize the environment of work in the system
     """
 
-    def __init__(self, oscBack="000", oscPlot="", oscGridX=True, oscGridY=True, minYOsc = -100,maxYOsc = 100, minYSpec = 0,maxYSpec = 22, powBack="000", powPlot="FFF", powGridX=True,
-                 powGridY=True, specBack="000", specGridX=True, specGridY=True, colorbar=None, region=None, endColor="FFF", centerColor="FFF", startColor="FFF",
-                 quart1Color="FFF", quart2Color="FFF"):
-        self.osc_background = oscBack
-        self.osc_plot = oscPlot
-        self.osc_GridX = oscGridX
-        self.osc_GridY = oscGridY
-        self.spec_GridX = specGridX
-        self.spec_GridY = specGridY
-        self.minYOsc = minYOsc
-        self.maxYOsc  = maxYOsc
-        self.minYSpec = minYSpec
-        self.maxYSpec  = maxYSpec
-        self.pow_Back = powBack
-        self.pow_Plot = powPlot
-        self.pow_GridX = powGridX
-        self.pow_GridY = powGridY
-        self.spec_background = specBack
-        self.colorBarState = colorbar
-        self.histRange = region
-        self.endColor = endColor
-        self.startColor = startColor
-        self.quart1Color = quart1Color
-        self.centerColor = centerColor
-        self.quart2Color = quart2Color
+    def __init__(self, oscillogramTheme=None, spectrogramTheme=None, powerSpectrumTheme=None):
+                 #, endColor="FFF", centerColor="FFF", startColor="FFF", quart1Color="FFF", quart2Color="FFF"):
+        self.oscillogramTheme = oscillogramTheme if oscillogramTheme is not None else OscillogramTheme()
+        self.spectrogramTheme = spectrogramTheme if spectrogramTheme is not None else SpectrogramTheme()
+        self.powerSpectrumTheme = powerSpectrumTheme if powerSpectrumTheme is not None else PowerSpectrumTheme()
+        # self.endColor = endColor
+        # self.startColor = startColor
+        # self.quart1Color = quart1Color
+        # self.centerColor = centerColor
+        # self.quart2Color = quart2Color
