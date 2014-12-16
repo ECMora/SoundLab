2e144cb491abbdaf8561f808909c56d3862b70cf
y.febles <y.febles@citmatel.inf.cu>
2014-12-16 16:19:06 -0500

    start of one dim transformation improvement

---
+++ graphic_interface/widgets/DockWidgetExpandable.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from PyQt4 import QtGui, QtCore
+
+
+class DockWidgetExpandable(QtGui.QDockWidget):
+    """
+    Widget that represent a dock window that can be
+    opened by moving mouse on it.
+    change it size automatically with the mouse move event.
+    """
+    pass
