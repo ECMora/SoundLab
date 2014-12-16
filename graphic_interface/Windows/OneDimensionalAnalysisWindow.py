e99bf8a679046f311cf774354d400772b9a91637
y.febles <y.febles@citmatel.inf.cu>
2014-12-16 16:21:16 -0500

    one dim windows update

--- graphic_interface/Windows/OneDimensionalAnalysisWindow.py
+++ graphic_interface/Windows/OneDimensionalAnalysisWindow.py
@@ -1,92 +1,145 @@
 # -*- coding: utf-8 -*-
-from PyQt4.QtCore import pyqtSlot
-from graphic_interface.widgets.Power_Spectrum  import Ui_PowSpecWindow
 from PyQt4 import QtGui
-from graphic_interface.windows.ParameterList import DuettoListParameterItem
-from pyqtgraph.parametertree.parameterTypes import WidgetParameterItem,ListParameter
+from pyqtgraph.parametertree.parameterTypes import ListParameter
 from pyqtgraph.parametertree import Parameter, ParameterTree
+from graphic_interface.windows.one_dim_transforms_window import Ui_OneDimensionalWindow
+from graphic_interface.windows.ParameterList import DuettoListParameterItem
+from graphic_interface.one_dimensional_transforms.OneDimensionalTransforms import *
 
 
-class PowerSpectrumWindow(QtGui.QMainWindow,Ui_PowSpecWindow):
-    def __init__(self,parent=None,lines=True,data=[],range=[],NFFTSpec=0,theme=None,rate=2, bitdepth=0, updateInterval=lambda:None):
-        super(PowerSpectrumWindow, self).__init__(parent)
-        self.ui = Ui_PowSpecWindow()
-        self.ui.setupUi(self)
-        self.ui.widget.setData(data, range, bitdepth, rate, NFFTSpec, updateInterval)
+class OneDimensionalAnalysisWindow(QtGui.QMainWindow, Ui_OneDimensionalWindow):
+    """
+    Window that allow to create and visualize one dimensional transforms on signals
+    """
+    DOCK_OPTIONS_WIDTH = 250
+
+    def __init__(self,parent=None):
+        super(OneDimensionalAnalysisWindow, self).__init__(parent)
+        self.setupUi(self)
         self.show()
-        self.ui.widget.backColor = theme.pow_Back
-        self.ui.widget.plotColor = theme.pow_Plot
-        self.ui.widget.gridX = theme.pow_GridX
-        self.ui.widget.gridY = theme.pow_GridY
-        self.ui.widget.PointerChanged.connect(self.updateStatusBar)
-        self.ui.widget.getPlotItem().hideButtons()
-        self.ui.dock_settings_contents.setStyleSheet("background-color:#DDF")
-        #Parameter Tree Settings
 
-        params = self.ui.widget.getParamsList()
+        # connect the tool detected data to show the status bar message
+        self.statusbar = self.statusBar()
+        self.statusbar.setSizeGripEnabled(False)
+        self.widget.toolDataDetected.connect(self.updateStatusBar)
 
-        wSettings = {u'name': unicode(self.tr(u'Window Settings')), u'type': u'group', u'children': [
-            {u'name': unicode(self.tr(u'Plot color')), u'type': u'color', u'value':self.ui.widget.plotColor},
-            {u'name': unicode(self.tr(u'Background color')), u'type': u'color', u'value':self.ui.widget.backColor},
-            {u'name': unicode(self.tr(u'Grid')), u'type': u'group', u'children':[
-                {u'name': unicode(self.tr(u'X')), u'type': u'bool', u'value': self.ui.widget.gridX},
-                {u'name': unicode(self.tr(u'Y')), u'type': u'bool', u'value':self.ui.widget.gridY}]},
-            # {u'name': u'Lines', u'type':u'bool', u'value': self.ui.widget.lines},
-        ]}
-        params.append(wSettings)
+        # Parameter Tree Settings
+        self.__createParameterTree()
+
+    def load_Theme(self, theme):
+        """
+        Load the supplied theme into the widget inside.
+        :param theme: The theme with the visual options
+        :return:
+        """
+        self.widget.load_Theme(theme)
+
+    # region Graph
+
+    def graph(self):
+        self.widget.graph()
+
+    def updateGraph(self, indexFrom, indexTo):
+        """
+        Update the graph of the one dimensional
+        selected transformation in the signal interval supplied.
+        Apply the transformation to the signal interval and plot it.
+        :param indexFrom: start of the interval in signal data indexes
+        :param indexTo: end of the interval in signal data indexes
+        :return:
+        """
+        pass
+
+    # endregion
+
+    # region Parameter Tree Options
+
+    def __createParameterTree(self):
+        """
+        Create the ParameterTree with the options of the window.
+        The ParameterTree contains the combo box of
+        the active one dimensional transforms to select.
+        :return:
+        """
+        params = [
+            {u'name': unicode(self.tr(u'One_Dim_Transform')), u'type': u'list',
+             u'value': u"Envelope",
+             u'default': Envelope,
+             # classes of the one dim transforms. to add a new one
+             # just add it to the list and implement it
+             u'values': [(u'Envelope', Envelope),
+                         (u'AveragePowSpec', AveragePowSpec),
+                         (u'LogarithmicPowSpec', LogarithmicPowSpec),
+                         (u'InstantaneousFrequencies', InstantaneousFrequencies)
+             ]}
+        ]
+
         ListParameter.itemClass = DuettoListParameterItem
         self.ParamTree = Parameter.create(name=u'params', type=u'group', children=params)
-        self.ParamTree.sigTreeStateChanged.connect(self.windowSettingsChange)
-        self.ui.widget.connectSignals(self.ParamTree)
 
+        # create and set initial properties
         self.parameterTree = ParameterTree()
         self.parameterTree.setAutoScroll(True)
-        self.parameterTree.setFixedWidth(250)
-
+        self.parameterTree.setFixedWidth(self.DOCK_OPTIONS_WIDTH)
         self.parameterTree.setHeaderHidden(True)
         self.parameterTree.setParameters(self.ParamTree, showTop=False)
 
-        lay1 = QtGui.QVBoxLayout()
-        lay1.setMargin(0)
-        lay1.addWidget(self.parameterTree)
+        # connect the signals to react when a change of transform is made
+        self.ParamTree.param(unicode(self.tr(u'One_Dim_Transform'))).sigValueChanged.connect(self.changeTransform)
 
-        self.ui.dock_settings_contents.setLayout(lay1)
-        self.ui.dockSettings.setVisible(False)
-        self.ui.dockSettings.setFixedWidth(250)
+        # set a default one dim transform to the widget
+        self.widget.transform = Envelope()
 
-    def windowSettingsChange(self, param, changes):
-        for param, change, data in changes:
-            path = self.ParamTree.childPath(param)
-            if path is not None:
-                childName = '.'.join(path)
-            else:
-                childName = param.name()
+        # reload the new widgets transform options
+        self.reloadOptionsWidget(self.widget.transform)
 
-            if childName == unicode(self.tr(u'Window Settings.Plot color')):
-                self.ui.widget.plotColor = data
-            elif childName == unicode(self.tr(u'Window Settings.Background color')):
-                self.ui.widget.backColor = data
-                self.ui.widget.setBackground(data)
-                return
-            elif childName == unicode(self.tr(u'Window Settings.Grid.X')):
-                self.ui.widget.gridX = data
-                self.ui.widget.showGrid(x=data, y=self.ui.widget.gridY)
-                return
-            elif childName == unicode(self.tr(u'Window Settings.Grid.Y')):
-                self.ui.widget.gridY = data
-                self.ui.widget.showGrid(x=self.ui.widget.gridX, y=data)
-                return
-            else: return
-            self.ui.widget.refresh()
+    def reloadOptionsWidget(self, one_dim_transform):
+        """
+        Refresh the parameter tree and the layout
+        of the options windows when a change of  transformation is made.
+        removes the old transformation options and set the parameter tree
+        of the new one.
+        :param one_dim_transform: the new transform.
+        :return:
+        """
 
-    @pyqtSlot()
-    def on_actionOneDimFunctSettings_triggered(self):
-        if self.ui.dockSettings.isVisible():
-            self.ui.dockSettings.setVisible(False)
-        else: self.ui.dockSettings.setVisible(True)
+        options_window_layout = QtGui.QVBoxLayout()
+        options_window_layout.setMargin(0)
+        options_window_layout.addWidget(self.parameterTree)
+        options_window_layout.setStretchFactor(self.parameterTree,0)
 
-    def updateStatusBar(self,message):
-        self.ui.statusbar.showMessage(message, 5000)
+        # add the parameter tree of the transform if exists
+        if one_dim_transform is not None:
+            options_window_layout.addWidget(one_dim_transform.settings)
 
-    def updatePowSpectrumInterval(self,range):
-        self.ui.widget.updateLast(range)
+        # removing the old layout from the dock widget
+        self.dock_settings_contents = QtGui.QWidget()
+        self.dock_settings_contents.setLayout(options_window_layout)
+        self.dockSettings.setWidget(self.dock_settings_contents)
+
+        self.dockSettings.setVisible(True)
+        self.dockSettings.setFixedWidth(self.DOCK_OPTIONS_WIDTH)
+
+    def changeTransform(self, parameter):
+        """
+        Method invoked when a new transform is selected.
+        Change the transform in the widget and update the options window
+        :param parameter: the parameter tree node that change
+        :return:
+        """
+
+        transform_class = parameter.value()
+
+        self.widget.transform = transform_class()
+
+        self.reloadOptionsWidget(self.widget.transform)
+
+    # endregion
+
+    def updateStatusBar(self, line):
+        """
+        Update the status bar window message.
+        :param line: The (string) to show as message
+        :return: None
+        """
+        self.statusbar.showMessage(line)
